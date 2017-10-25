9016551251a7cfde426c6c914da92fe1ec3907be
Kayla Ford <Kayla.ford@mail.citytech.cuny.edu>
2017-10-25 16:01:37 -0400

    handlebar changes

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 'use strict';
 const express = require('express')
 const app = express();
+var path = require('path');
+var exphbs = require('express-handlebars');
 
+app.engine('handlebars', exphbs({layoutsDir: path.join(__dirname, "views/layout"),
+  partialsDir: path.join(__dirname, "views/partials"),
+  defaultLayout: 'index',
+  extname: 'handlebars'}));
+ app.set('view engine', 'handlebars');
 app.get('/', function (req, res) {
-  
-  res.render('index');
+
+  res.render('layout/index');
 })
 
-//app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'ejs');
-
+app.set('views', path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, 'public')));
 app.listen(8000, function () {
   console.log('Local host 8000')
 })
