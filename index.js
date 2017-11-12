9f51dfa7a09033a7e1233e309af4405f2327d8d7
Kayla Ford <Kayla.ford@mail.citytech.cuny.edu>
2017-11-12 18:45:52 -0500

    new changes

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 const express = require('express')
 const app = express();
 var path = require('path');
+var React = require('react');
 var exphbs = require('express-handlebars');
+
 
 app.engine('handlebars', exphbs({layoutsDir: path.join(__dirname, "views/layout"),
   partialsDir: path.join(__dirname, "views/partials"),
@@ -12,13 +14,17 @@ app.engine('handlebars', exphbs({layoutsDir: path.join(__dirname, "views/layout"
 app.get('/', function (req, res) {
 
   res.render('layout/index');
-})
+});
+app.get('/test', function (req, res) {
+
+  res.sendfile('public/UserHome.html');
+});
 
 app.set('views', path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, 'public')));
 app.listen(8000, function () {
   console.log('Local host 8000')
-})
+});
 
 
 module.exports = app;
