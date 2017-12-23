43a8df1392e2f3ca49c5cc82d32627ba1330859c
Kayla Ford <Kayla.ford@mail.citytech.cuny.edu>
2017-12-23 13:32:43 -0500

    more changes

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.get('/test', function (req, res) {
 
 app.set('views', path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, 'public')));
-app.listen(process.env.PORT || 5000), function () {
+app.listen(process.env.PORT || 5000, function () {
   console.log('Local host 8000')
 });
 
